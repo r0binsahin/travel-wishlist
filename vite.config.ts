16c72c29161dc1348c50c15e155e8df7eab8bb16
r0binsahin <robin.sahin88@gmail.com>
2024-07-15 15:04:01 +0200

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/travel-wishlist/',
+  base: '/travel-wishlist',
   build: {
     target: 'esnext',
   },
